2ff9974c763ff2fb725a7711a59bbb00a6de16ef
Only Huân <huannd0101@gmail.com>
2021-05-13 14:34:13 +0700

    ...

--- app/src/main/java/com/example/eathit/Main2Activity.java
+++ app/src/main/java/com/example/eathit/Main2Activity.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -41,6 +46,25 @@ public class Main2Activity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main2);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
+//        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+//            @Override
+//            public void onDestinationChanged(@NonNull NavController controller,
+//                                             @NonNull NavDestination destination, @Nullable Bundle arguments) {
+//                if(destination.getId() == R.id.full_screen_destination) {
+//                    toolbar.setVisibility(View.GONE);
+//                    bottomNavigationView.setVisibility(View.GONE);
+//                } else {
+//                    toolbar.setVisibility(View.VISIBLE);
+//                    bottomNavigationView.setVisibility(View.VISIBLE);
+//                }
+//            }
+//        });
+
     }
 
     @Override
