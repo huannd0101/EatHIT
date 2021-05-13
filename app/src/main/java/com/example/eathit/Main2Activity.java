2d9bb4261b54022a63fb9f76dee194c9fd39dd21
Only Huân <huannd0101@gmail.com>
2021-05-13 14:50:28 +0700

    .

--- app/src/main/java/com/example/eathit/Main2Activity.java
+++ app/src/main/java/com/example/eathit/Main2Activity.java
@@ -1,19 +1,17 @@
 package com.example.eathit;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.navigation.NavController;
-import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -46,34 +44,50 @@ public class Main2Activity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main2);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
-
+        //button navigation
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
         NavigationUI.setupWithNavController(bottomNav, navController);
+        //bắt đầu code từ đây nhé :v bên trên là config drawer navigation và button navigation
 
-//        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
-//            @Override
-//            public void onDestinationChanged(@NonNull NavController controller,
-//                                             @NonNull NavDestination destination, @Nullable Bundle arguments) {
-//                if(destination.getId() == R.id.full_screen_destination) {
-//                    toolbar.setVisibility(View.GONE);
-//                    bottomNavigationView.setVisibility(View.GONE);
-//                } else {
-//                    toolbar.setVisibility(View.VISIBLE);
-//                    bottomNavigationView.setVisibility(View.VISIBLE);
-//                }
-//            }
-//        });
 
     }
 
+
+
+
+
+
+
+
+
+
+    //tạo menu :v
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main2, menu);
         return true;
     }
 
+    //xử lý khi click vào menu
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_chats:
+                Toast.makeText(getApplicationContext(), "Chats", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_settings:
+                Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_helps:
+                Toast.makeText(getApplicationContext(), "hiếp", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //navigation...
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main2);
